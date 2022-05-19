ca3969a5bde9237f8cf30b957964b8ced8f083fc
AZor1973 <azor@mail.ru>
2022-05-19 10:22:48 +0300

    Initial commit

---
+++ src/main/java/ru/azor/hibernate/repository/HibernateRepositoryApplication.java
@@ -0,0 +1,13 @@
+package ru.azor.hibernate.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateRepositoryApplication.class, args);
+    }
+
+}
